90439998591055cc46ddbccb6b035d5db1199690
Dinkar Ganti <dinkarg@localhost.localdomain>
2018-07-25 19:58:26 -0400

    License files etc.

--- prototype/haskell/src/Realitz/Tezos/RPC/Operations/InternalResultStatus.hs
+++ prototype/haskell/src/Realitz/Tezos/RPC/Operations/InternalResultStatus.hs
@@ -1,28 +1,31 @@
+--***************************************************************************--
+--                                                                           --
+-- Open Source License                                                       --
+-- Copyright (c) Dinkar Ganti, dinkar.ganti@gmail.com                        --
+--                                                                           --
+-- Permission is hereby granted, free of charge, to any person obtaining a   --
+-- copy of this software and associated documentation files (the "Software"),--
+-- to deal in the Software without restriction, including without limitation --
+-- the rights to use, copy, modify, merge, publish, distribute, sublicense,  --
+-- and/or sell copies of the Software, and to permit persons to whom the     --
+-- Software is furnished to do so, subject to the following conditions:      --
+--                                                                           --
+-- The above copyright notice and this permission notice shall be included   --
+-- in all copies or substantial portions of the Software.                    --
+--                                                                           --
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR--
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  --
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   --
+-- THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER--
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   --
+-- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       --
+-- DEALINGS IN THE SOFTWARE.                                                 --
+--                                                                           --
+--***************************************************************************--
+
 module Realitz.Tezos.RPC.Operations.InternalResultStatus where 
 import Realitz.Tezos.RPC.Operations.ResultStatus
 import Realitz.Tezos.RPC.Common
-
-{-| 
--- @
-  case class AppliedInternalOperationResult(
-                                           kind: String,
-                                           source: String,
-                                           nonce: Int,
-                                           publicKey: Option[String],
-                                           result: AppliedOperationResultStatus,
-                                           amount: Option[Int],
-                                           destination: Option[String],
-                                           parameters: Option[Any],
-                                           managerPubKey: Option[String],
-                                           balance: Option[Int],
-                                           spendable: Option[Boolean],
-                                           delegatable: Option[Boolean],
-                                           delegate: Option[String],
-                                           script: Option[ScriptedContracts],
-                                           )
-
---@ 
--}
 
 data InternalResultStatus = InternalResultStatus {
     kind :: UpdateKind
--- src/haxe/realitz/tezos/rpc/BlockHeader.hx
+++ src/haxe/realitz/tezos/rpc/BlockHeader.hx
@@ -1,7 +1,33 @@
 package realitz.tezos.rpc;
 
+/*****************************************************************************/
+/*                                                                           */
+/* Open Source License                                                       */
+/* Copyright (c) Dinkar Ganti, dinkar.ganti@gmail.com */
+/*                                                                           */
+/* Permission is hereby granted, free of charge, to any person obtaining a   */
+/* copy of this software and associated documentation files (the "Software"),*/
+/* to deal in the Software without restriction, including without limitation */
+/* the rights to use, copy, modify, merge, publish, distribute, sublicense,  */
+/* and/or sell copies of the Software, and to permit persons to whom the     */
+/* Software is furnished to do so, subject to the following conditions:      */
+/*                                                                           */
+/* The above copyright notice and this permission notice shall be included   */
+/* in all copies or substantial portions of the Software.                    */
+/*                                                                           */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*/
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   */
+/* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*/
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   */
+/* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       */
+/* DEALINGS IN THE SOFTWARE.                                                 */
+/*                                                                           */
+/*****************************************************************************/
+
 import haxe.io.Bytes;
 import haxe.Int64;
+import haxe.ds.Option;
 
 /** 
 Code walkthrough process: it is simpler and probably intended that 
@@ -269,7 +295,17 @@ enum PeerState {
 }
 
 class PeerStatistics {
-  var stats : String;
+  var totalSent : Int64;
+  var totalReceived : Int64;
+  var currentInflow : Int;
+  var currentOutflow : Int;
+}
+
+class Monitor {
+  var requestKind : PoolEvent;
+  var timeStamp : Int64;
+  var address : String;
+  var port : Int;
 }
 
 class Peer {
@@ -285,4 +321,8 @@ class Peer {
   var lastDisconnection : List<ConnectionInfo>;
   var lastSeen : List<ConnectionInfo>;
   var lastMiss : List<ConnectionInfo>;
+}
+
+class BlockValidator {
+  
 }
